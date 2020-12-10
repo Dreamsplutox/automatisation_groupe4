b4d1a9128407b855283e9bf5f6eab6d4a6309297
Dreamsplutox <arnaudsimon091@gmail.com>
2020-12-10 02:01:00 +0100

    try with path in bash operator

--- test_airflow.py
+++ test_airflow.py
@@ -7,18 +7,18 @@ import sys
 sys.path.insert(0, "/root/airflow/dags/automatisation_groupe4/")
 
 dag = DAG(
-dag_id = ("group4_dag_prime"),
+dag_id = ("group4_dag"),
 start_date = datetime(2020, 12, 9),
 schedule_interval = timedelta(days=1))
  
 task1 = BashOperator(
 task_id = "hello_world",
-bash_command = "echo hello world",
+bash_command = "pwd",
 dag = dag)
  
 task2 = BashOperator(
 task_id = "execute_python",
-bash_command = "python hello.py",
+bash_command = "python /root/airflow/dags/automatisation_groupe4/hello.py",
 dag = dag)
 
 task1 >> task2
