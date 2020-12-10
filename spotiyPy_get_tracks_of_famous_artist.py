40e316b405d9581991ef69a6bb451abc38d74aff
Dreamsplutox <arnaudsimon091@gmail.com>
2020-12-10 01:03:12 +0100

    try to execute python script with airflow

---
+++ spotiyPy_get_tracks_of_famous_artist.py
@@ -0,0 +1,170 @@
+#imports
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import time
+import datetime
+import os
+
+
+
+# ### Get only usefull playlists for the current year (current year + "top" in their name) 
+def getUsefullPlaylists(dictPlayLists):
+    ok_playlists = []
+    
+    for i in range(0, len(dictPlayLists)): 
+        try:
+            if "Top" in dictPlayLists[i]["playListName"] and str(current_year) in dictPlayLists[i]["playListName"]:
+                ok_playlists.append(dictPlayLists[i])
+        except KeyboardInterrupt:
+            print("keyboard interrupt, leaving")
+            os._exit(0)
+        except:
+            print("an error has occured in get usefull playlist")
+            continue
+        
+    return ok_playlists
+
+# ### Get all track ids (+ kill dupplicates)
+def getTrackIDs(user, playlist_id):
+    ids = []
+    playlist = sp.user_playlist(user, playlist_id)
+    for item in playlist['tracks']['items']:
+        try:
+            track = item['track']
+            ids.append(track['id'])
+        except KeyboardInterrupt:
+            print("keyboard interrupt, leaving")
+            os._exit(0)
+        except:
+            print("an error has occured in get track ids")
+            continue
+    return ids
+
+
+# ### Get usefull data (artists ids and names) in all the tracks + kill dupplicate artist
+def getTrackFeatures(id):
+    meta = sp.track(id)
+    features = sp.audio_features(id)
+
+    # meta²
+    name = meta['name']
+    album = meta['album']['name']
+    artist = meta['album']['artists'][0]['name']
+    artist_id = meta['album']['artists'][0]['id']
+    release_date = meta['album']['release_date']
+    length = meta['duration_ms']
+    popularity = meta['popularity']
+
+    # features
+
+    track = [name, album, artist, artist_id, release_date, length, popularity]
+    return track
+
+
+if __name__ == "__main__":
+    #credentials
+    clientId= "758b9e90346649409515e5705043464f"
+    clientSecret="4130541e7efd4a1c912ac4571909484b"
+    client_credentials_manager = SpotifyClientCredentials(clientId, clientSecret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+    # ### If we are in january, set the year to current year -  1
+    current_year = datetime.date.today().year
+    current_month = datetime.date.today().month
+    if current_month == 1 :
+        current_year = current_year - 1
+
+
+    # ### First get as many playlist as possible (let's say, 100)
+
+    dictPlayLists = []
+    for i in range(0,60):
+        try:
+            track_results = sp.search(q='year:'+str(current_year), type='playlist', limit=1,offset=i)
+            tmpDict = None
+            tmpDict = {
+                'idPlayList':track_results['playlists']['items'][0]['id'],
+                'playListName':track_results['playlists']['items'][0]['name']
+            }
+            dictPlayLists.append(tmpDict)
+        except KeyboardInterrupt:
+            print("keyboard interrupt, leaving")
+            os._exit(0)
+        except:
+            print("an error has occured in dict playlist")
+
+    dictPlayLists = getUsefullPlaylists(dictPlayLists)
+
+    ids = []
+
+    for elem in dictPlayLists:
+        try:
+            ids += getTrackIDs(elem["playListName"], elem["idPlayList"])
+        except KeyboardInterrupt:
+             print("keyboard interrupt, leaving")
+             os._exit(0)
+        except:
+            print("an error has occured in get usefull playlists")
+            continue
+
+    #ids = getTrackIDs('Top Hits 2020', '53w0lVHBw0m4eEz54yN8FH')
+
+
+    # kill dupplicates
+    ids = list(dict.fromkeys(ids))
+
+    # loop over track ids 
+    tracks = []
+    all_artist_ids = []
+
+    for i in range(len(ids)):
+        try:
+            #time.sleep(.5)
+            track = getTrackFeatures(ids[i])
+            # check if id of the artist is already here, if that's case don't append the track
+            if track[3] not in all_artist_ids:
+                # don't add
+                all_artist_ids.append(track[3])
+                tracks.append(track)
+        except KeyboardInterrupt:
+            print("keyboard interrupt, leaving")
+            os._exit(0)
+        except:
+            print("an error has occured, go to next track (collect artist phase)")
+            continue   
+
+    # ### CREATE / ( maybe MERGE later) CSV with what we have
+    # create dataset
+    df = pd.DataFrame(tracks, columns = ['name', 'album', 'artist','artist_id', 'release_date', 'length', 'popularity'])
+
+    artist_ids = df["artist_id"].tolist()
+    artist_names = df["artist"].tolist()
+
+    #init dict track_artists ==> will contain all songs of famous artists
+    track_artists = []
+
+    for i in range(0, len(artist_ids)):
+        tracks = sp.artist_top_tracks(artist_ids[i])
+        for track in tracks['tracks'] :
+            try:
+                track_artists.append([artist_ids[i], artist_names[i], track['name'], track['album']['release_date'], track['popularity']])
+            except KeyboardInterrupt:
+                print("keyboard interrupt, leaving")
+                os._exit(0)
+            except:
+                print("an error has occured, go to next track")
+                continue
+
+    # ### Create the dataset
+    df = pd.DataFrame(track_artists, columns = ['artist_id', "artist_name", 'track_name', 'track_release_date', 'track_popularity'])
+    df.to_csv("track_artists_"+str(datetime.date.today())+".csv", sep = ';', encoding="utf-8-sig", index=False)
+    #df.to_csv("spotify_famous_artists.csv", sep = ',', encoding="utf-8-sig", index=False)
+
+    #HDFS part
+    source = "track_artists_"+str(datetime.date.today())+".csv"
+    destination = "/user/iabd2_group4"
+    command = "hdfs dfs -copyFromLocal " + source + " " + destination
+    print(command)
+    #os.system(command)
--- test_airflow.py
+++ test_airflow.py
@@ -1,23 +1,19 @@
-from airflow import DAG
+from airflow import DAG, macros
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
 dag = DAG(
-dag_id = ("toto_2020_12_09"),
+dag_id = ("group4_dag"),
 start_date = datetime(2020, 12, 9),
 schedule_interval = timedelta(days=1))
  
 task1 = BashOperator(
-task_id = "toto1",
+task_id = "hello_world",
 bash_command = "echo hello world",
 dag = dag)
  
 task2 = BashOperator(
-task_id = "toto2",
-bash_command = "echo {{ ds }}",
-dag = dag)
- 
-task3 = BashOperator(
-task_id = "toto3",
-bash_command = "sleep 5",
+task_id = "execute_python",
+bash_command = "python spotiyPy_get_tracks_of_famous_artist.py",
 dag = dag)
